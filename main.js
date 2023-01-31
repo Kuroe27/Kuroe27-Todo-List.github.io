6de89499460294097d992f35abb86e7022b02155
Leo Mikko Uson <105025585+Kuroe27@users.noreply.github.com>
2023-01-31 12:51:18 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,30 +5,10 @@ add.addEventListener("click", () => {
     b.className = 'box';
     b.innerHTML = `
     <div class='tools'>
-     <button class='max'>/</button>
         <button class='rem'>X</button>
     </div>
     <textarea class='rem'></textarea>`;
     b.querySelector('.rem').addEventListener("click", () => b.remove());
-
-    b.querySelector('.max').addEventListener("click", () => {
-        b.classList.toggle("maxi")
-        b.style.zIndex = "2"
-        const boxes = document.querySelectorAll(".box");
-        boxes[i].style.display = "block";
-        for (let i = 0; i < boxes.length; i++) {
-            if (boxes[i] !== b) {
-                if (b.classList.contains("maxi")) {
-                    boxes[i].classList.remove("maxi");
-                    boxes[i].style.display = "none";
-                } else {
-                    boxes[i].style.display = "block";
-                }
-            }
-        }
-    });
-
-
 });
 
 
